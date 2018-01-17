d19a217d1f3118967f6696849e74b37e212df857
bianzhaiwang <bianzhaiwang@quarkfinance.com>
2018-01-17 14:50:39 +0800

    checkin

---
+++ MouseSync/MouseSync/Code/AxAcroPDF.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseSync.Code
+{
+    public class AxAcroPDF
+    {
+        public AxAcroPDFLib.AxAcroPDF AcroPDF { get; set; }
+
+        public Dictionary<string, object> _Options { get; set; }
+
+        public AxAcroPDF(Dictionary<string, object> options)
+        {
+            _Options = options;
+        }
+
+        public void OpenFile(string fileName)
+        {
+            AcroPDF.src = fileName;
+        }
+
+        public void InitAxFramerControl(System.Windows.Forms.Integration.WindowsFormsHost windowsFormsHost)
+        {
+            AcroPDF = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(AcroPDF)).BeginInit();
+
+            AcroPDF.Enabled = true;
+            AcroPDF.Name = _Options.GetStringValue("Name");
+            AcroPDF.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            windowsFormsHost.Child = AcroPDF;
+
+            ((System.ComponentModel.ISupportInitialize)(AcroPDF)).EndInit();
+        }
+    }
+}
--- MouseSync/MouseSync/Window2.xaml.cs
+++ MouseSync/MouseSync/Window2.xaml.cs
@@ -26,17 +26,33 @@ namespace MouseSync
             InitializeComponent();
         }
         Code.AxFramerControl _AxFramerControl;
+
+        Code.AxAcroPDF _AxAcroPDF { get; set; }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Dictionary<string, object> _AxFramerOptions = new Dictionary<string, object>();
-            _AxFramerOptions.Add("Name", "AxFramerControl1");
+           // Dictionary<string, object> _AxFramerOptions = new Dictionary<string, object>();
+           // _AxFramerOptions.Add("Name", "AxFramerControl1");
 
-           _AxFramerControl = new Code.AxFramerControl(_AxFramerOptions);
-            _AxFramerControl.InitAxFramerControl(WindowsFormsHost1);
+           //_AxFramerControl = new Code.AxFramerControl(_AxFramerOptions);
+           // _AxFramerControl.InitAxFramerControl(WindowsFormsHost1);
 
-            _AxFramerControl.OpenFile(AppDomain.CurrentDomain.BaseDirectory + "风险评估问卷CRM实体设计.docx");
+           // _AxFramerControl.OpenFile(AppDomain.CurrentDomain.BaseDirectory + "风险评估问卷CRM实体设计.docx");
 
+            try
+            {
+                Dictionary<string, object> _AxAcroPDFOptions = new Dictionary<string, object>();
+                _AxAcroPDFOptions.Add("Name", "AxAcroPDFOptions1");
 
+                _AxAcroPDF = new Code.AxAcroPDF(_AxAcroPDFOptions);
+                _AxAcroPDF.InitAxFramerControl(WindowsFormsHost1);
+
+                _AxAcroPDF.OpenFile(AppDomain.CurrentDomain.BaseDirectory + "麦肯锡精英的48个工作习惯10.pdf");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
@@ -50,7 +66,7 @@ namespace MouseSync
 
             //_AxFramerControl2.OpenFile(AppDomain.CurrentDomain.BaseDirectory + "风险评估问卷CRM实体设计_1.docx");
 
-            Microsoft.Office.Interop.Word.Document doc = _AxFramerControl.FramerControl.ActiveDocument as Microsoft.Office.Interop.Word.Document;
+            //Microsoft.Office.Interop.Word.Document doc = _AxFramerControl.FramerControl.ActiveDocument as Microsoft.Office.Interop.Word.Document;
 
             List<IntPtr> list = new List<IntPtr>();
             Code.CallBack myCallBack = new Code.CallBack((h, p) =>
@@ -58,7 +74,7 @@ namespace MouseSync
                 list.Add(h);
                 return true;
             });
-            Code.Win32Api.EnumWindows(myCallBack, doc.ActiveWindow.Hwnd);
+            Code.Win32Api.EnumWindows(myCallBack, 0);
 
             List<int> posList = new List<int>();
             List<SCROLLINFO> scrollInfoList = new List<SCROLLINFO>();
@@ -78,7 +94,21 @@ namespace MouseSync
             if (posList.Count > 0)
             {
             }
+
+            SCROLLINFO si2 = new SCROLLINFO();
+            si2.cbSize = (uint)Marshal.SizeOf(si2);
+            si2.fMask = (int)ScrollInfoMask.SIF_ALL;
+            bool result2 = GetScrollInfo(_AxAcroPDF.AcroPDF.Handle, 1, ref si2);
+            if (result2) scrollInfoList.Add(si2);
+
+            PostMessage(_AxAcroPDF.AcroPDF.Handle, WM_VSCROLL, MakeLong((short)SB_THUMBTRACK, (short)(100)), 0);
+
+            this._AxAcroPDF.AcroPDF.Focus();
+            mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -100, 0);
+            mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -100, 0);
         }
+
+        const int MOUSEEVENTF_WHEEL = 0x800;
 
         [DllImport("user32.dll", EntryPoint = "GetScrollPos")]
         public static extern int GetScrollPos(IntPtr hwnd, int nBar);
@@ -91,5 +121,22 @@ namespace MouseSync
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
         public static extern IntPtr GetForegroundWindow();
+
+        public static int MakeLong(short lowPart, short highPart)
+        {
+            return (int)(((ushort)lowPart) | (uint)(highPart << 16));
+        }
+        public const int SB_THUMBTRACK = 5;
+        public const int WM_HSCROLL = 0x114;
+        public const int WM_VSCROLL = 0x115;
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto, EntryPoint = "SendMessage")]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr PostMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int flags, int dX, int dY, int buttons, int extraInfo);
+
     }
 }
