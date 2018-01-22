271bd236f2d54c84ed1796bde21de377db6d3783
bianzhaiwang <bianzhaiwang@quarkfinance.com>
2018-01-22 16:26:42 +0800

    checkin

---
+++ DataDistribution/DataDistribution.Service/Code/DataStore.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataDistribution.Service.Code
+{
+    public class DataStore
+    {
+        static ConcurrentDictionary<string, ConcurrentDictionary<long,ConcurrentQueue<byte[]>>> _DataDictionary { get; set; }
+
+        static DataStore()
+        {
+            _DataDictionary = new ConcurrentDictionary<string, ConcurrentDictionary<long, ConcurrentQueue<byte[]>>>();
+        }
+
+        public static bool SetData(string dataKey, List<long> userIdList, byte[] data)
+        {
+            if (data == null || userIdList == null || userIdList.Count == 0 || string.IsNullOrEmpty(dataKey)) return false;
+
+            ConcurrentDictionary<long, ConcurrentQueue<byte[]>> userDict = null;
+            if (!_DataDictionary.ContainsKey(dataKey))
+            {
+                userDict = new ConcurrentDictionary<long, ConcurrentQueue<byte[]>>();
+                _DataDictionary[dataKey] = userDict;
+            }
+            else userDict = _DataDictionary[dataKey];
+
+            userIdList.ForEach(userId => AddUserData(userId, userDict, data));
+
+            return true;
+        }
+
+        static void AddUserData(long userId, ConcurrentDictionary<long, ConcurrentQueue<byte[]>> userDict, byte[] data)
+        {
+            ConcurrentQueue<byte[]> dataList = null;
+            if (userDict.ContainsKey(userId)) dataList = userDict[userId];
+            else { dataList = new ConcurrentQueue<byte[]>(); userDict[userId] = dataList; }
+
+            dataList.Enqueue(data);
+        }
+
+        public static byte[] GetData(string dataKey, long userId)
+        {
+            if (_DataDictionary.ContainsKey(dataKey))
+            {
+                if (_DataDictionary[dataKey].ContainsKey(userId))
+                {
+                    ConcurrentQueue<byte[]> dataList = _DataDictionary[dataKey][userId];
+                    byte[] data = null;
+                    if (dataList.TryDequeue(out data)) return data;
+                }
+            }
+
+            return null;
+        }
+
+        public static bool Remove(string dataKey)
+        {
+            if (_DataDictionary.ContainsKey(dataKey))
+            {
+                ConcurrentDictionary<long, ConcurrentQueue<byte[]>> userDict = null;
+                _DataDictionary.TryRemove(dataKey, out userDict);
+            }
+
+            return true;
+        }
+    }
+}
--- DataDistribution/DataDistribution.Service/Controllers/DefaultController.cs
+++ DataDistribution/DataDistribution.Service/Controllers/DefaultController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -18,7 +19,7 @@ namespace DataDistribution.Service.Controllers
         /// OPTIONS请求
         /// </summary>
         /// <returns></returns>
-        public string Options()
+        public Task<HttpResponseMessage> Options()
         {
             return null;
         }
@@ -27,13 +28,31 @@ namespace DataDistribution.Service.Controllers
         /// GET请求
         /// </summary>
         /// <returns></returns>
-        public HttpResponseMessage Get()
+        public async Task<HttpResponseMessage> Get(string className, string methodName)
         {
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK)
+            return await Task<HttpResponseMessage>.Run(() => { return GetData(className, methodName); });
+        }
+
+        HttpResponseMessage GetData(string className, string methodName)
+        {
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            try
             {
-                Content = new ByteArrayContent(Convert.FromBase64String("R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAICTAEAOw=="))
-            };
-            response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/gif");
+                byte[] bs = Code.DataStore.GetData(className, long.Parse(methodName));
+                if (bs != null) response.Content = new StreamContent(new MemoryStream(bs));
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            }
+            catch (Exception ex)
+            {
+                ex = Code.Common.GetInnerException(ex);
+
+                response.Content = new StringContent(ex.Message, Encoding.UTF8, "text/html");
+
+                response.StatusCode = HttpStatusCode.InternalServerError;
+
+                Code.LoggerProxy.Exception(className, methodName, ex);
+            }
 
             return response;
         }
@@ -44,20 +63,43 @@ namespace DataDistribution.Service.Controllers
         /// <param name="entityName"></param>
         /// <param name="methodName"></param>
         /// <returns></returns>
-        public async Task<JsonResult<object>> Post(string className, string methodName)
+        public async Task<HttpResponseMessage> Post(string className, string methodName)
         {
-            return await Task.Run<JsonResult<object>>(() => { return this.RequestAction(className, methodName); });
+            return await Task<HttpResponseMessage>.Run(() => { return PostData(className, methodName); });
         }
 
-        /// <summary>
-        /// 请求行为
-        /// </summary>
-        /// <param name="entityName"></param>
-        /// <param name="methodName"></param>
-        /// <returns></returns>
-        private JsonResult<object> RequestAction(string className, string methodName)
+        HttpResponseMessage PostData(string className, string methodName)
         {
-            return null;
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            try
+            {
+                if (methodName.ToLower().Equals("setdata")) SetData(className);
+                else if (methodName.ToLower().Equals("remove")) Code.DataStore.Remove(className); 
+            }
+            catch (Exception ex)
+            {
+                ex = Code.Common.GetInnerException(ex);
+
+                response.Content = new StringContent(ex.Message, Encoding.UTF8, "text/html");
+
+                response.StatusCode = HttpStatusCode.InternalServerError;
+
+                Code.LoggerProxy.Exception(className, methodName, ex);
+            }
+
+            return response;
+        }
+
+        void SetData(string dataKey)
+        {
+            byte[] data = this.Request.Content.ReadAsByteArrayAsync().Result;
+            if (data == null) return;
+
+            string idList = Code.Common.GetParameterValue(this.Request, "IdList");
+            List<long> userIdList = idList.Split(new char[] { ',', '，' }).Select(s => long.Parse(s)).ToList();
+
+            Code.DataStore.SetData(dataKey, userIdList, data);
         }
     }
 }
