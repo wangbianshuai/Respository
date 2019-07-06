925d4a0c73aee785bff6245aeded6d6ca38b567c
wanzuli <214935391@qq.com>
2019-07-07 00:11:10 +0800

    checkin

---
+++ SpringTest/src/main/java/com/example/TestApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Hello Spring Boot!";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
