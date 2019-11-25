04815911e4187cb6d882d621dd3922c99abd4dcb
wangbianzhai <wangbianzhai@wangbianzhaideMacBook-Air.local>
2019-11-25 14:00:41 +0800

    checkin

---
+++ WebHook/.umirc.js
@@ -0,0 +1,46 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  // routes: [
+  //   {
+  //     path: '/',
+  //     component: '../layouts/index',
+  //     routes: [
+  //       {
+  //         path: '/login',
+  //         component: './login',
+  //       },
+  //       {
+  //         path: '/user',
+  //         component: './user',
+  //       },
+  //       {
+  //         path: '/',
+  //         component: '../pages/index',
+  //       },
+  //     ],
+  //   },
+  // ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: false,
+        title: 'ReactTest',
+        dll: false,
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
+      },
+    ],
+  ],
+};
