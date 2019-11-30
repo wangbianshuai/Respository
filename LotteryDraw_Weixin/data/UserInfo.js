d87f67793c398af3f8b63fbe1ad53b10194e4670
wangbianzhai <277822695@qq.com>
2019-11-30 21:49:46 +0800

    checkin

--- LotteryDraw_Weixin/data/UserInfo.js
+++ LotteryDraw_Weixin/data/UserInfo.js
@@ -1,27 +1,35 @@
 var nameList = [
+  "张三",
+  "李四",
+  "王五",
+  "马六",
+  "张飞",
+  "赵云",
+  "关羽",
+  "刘备",
 ]
 
 //创建GUID
 function CreateGuid() {
-    var guid = ""
-    for (var i = 1; i <= 32; i++) {
-        var n = Math.floor(Math.random() * 16.0).toString(16)
-        guid += n
-        if ((i === 8) || (i === 12) || (i === 16) || (i === 20)) {
-            guid += "-"
-        }
+  var guid = ""
+  for (var i = 1; i <= 32; i++) {
+    var n = Math.floor(Math.random() * 16.0).toString(16)
+    guid += n
+    if ((i === 8) || (i === 12) || (i === 16) || (i === 20)) {
+      guid += "-"
     }
-    return guid
+  }
+  return guid
 }
 
 var data = nameList.map((name) => {
-    return {
-        UserId: CreateGuid(),
-        NickName: name,
-        AvatarUrl: ""
-    }
+  return {
+    UserId: CreateGuid(),
+    NickName: name,
+    AvatarUrl: ""
+  }
 })
 
 module.exports = {
-    DataList: data
+  DataList: data
 }
--- LotteryDraw_Weixin/pages/index/index.js
+++ LotteryDraw_Weixin/pages/index/index.js
@@ -3,42 +3,47 @@ var action = require('../../utils/action.js')
 var app = getApp()
 
 Page({
-    data: {
-        motto: '欢迎您，祝您好运！',
-        userInfo: {},
-        isAdmin: false
-    },
-    onShow: function () {
-        this.IsNavigateTo = false
-    },
-    //事件处理函数
-    bindTapLottery: function () {
-        if (this.IsNavigateTo) { return }
-        this.IsNavigateTo = true
+  data: {
+    motto: '欢迎您，祝您好运！',
+    userInfo: {},
+    isAdmin: false,
+    isHideAuth:true
+  },
+  onShow: function () {
+    this.IsNavigateTo = false
+  },
+  bindGetUserInfo: function () {
+    this.onLoad();
+  },
+  //事件处理函数
+  bindTapLottery: function () {
+    if (this.IsNavigateTo) { return }
+    this.IsNavigateTo = true
 
-        wx.navigateTo({ url: '../LotterySet/LotterySet' })
-    },
-    onLoad: function () {
-        var that = this
-        //调用应用实例的方法获取全局数据
-        app.getUserInfo(function (userInfo) {
-            var users = app.globalData.adminUsers.filter(function (item) { return item === userInfo.nickName })
-            var isAdmin = users.length > 0
-            //更新数据
-            that.setData({
-                userInfo: userInfo,
-                isAdmin: isAdmin
-            })
+    wx.navigateTo({ url: '../LotterySet/LotterySet' })
+  },
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      var users = app.globalData.adminUsers.filter(function (item) { return item === userInfo.nickName })
+      var isAdmin = users.length > 0
+      //更新数据
+      that.setData({
+        userInfo: userInfo,
+        isAdmin: isAdmin,
+        isHideAuth: true
+      })
 
-            action.SaveUser(userInfo, function (d) {
-                var motto = "签到成功，祝您好运！"
-                if (!d.Success) {
-                    motto = "签到失败，请稍后重新扫码签到"
-                }
-                that.setData({
-                    motto: motto,
-                })
-            })
+      action.SaveUser(userInfo, function (d) {
+        var motto = "签到成功，祝您好运！"
+        if (!d.Success) {
+          motto = "签到失败，请稍后重新扫码签到"
+        }
+        that.setData({
+          motto: motto,
         })
-    }
+      })
+    })
+  }
 })
