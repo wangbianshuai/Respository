f6fb9858fa54dfcff3932f906760e12573559e4c
wangbianzhai <277822695@qq.com>
2020-02-29 08:37:55 +0800

    checkin

--- StoryTestCases/Web/src/layouts/LeftRightLayout/Menu.js
+++ StoryTestCases/Web/src/layouts/LeftRightLayout/Menu.js
@@ -6,8 +6,6 @@ export default () => {
     return {
         NavMenuList: [
             GetStoryTestCasesNavMenu(),
-            GetProductManageNavMenu(),
-            GetPersonBillNavMenu(),
             GetSystemManageNavNenu(),
             GetPersonCenterNavMenu()
         ],
