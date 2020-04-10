5cd71e869c73ca9855e43d0fb61bff762d423e45
wangbianzhai <277822695@qq.com>
2020-04-10 22:14:54 +0800

    checkin

--- WebHook/src/tests/useAddCount.js
+++ WebHook/src/tests/useAddCount.js
@@ -1,19 +1,21 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useMemo } from "react";
 
 export default props => {
   const [count, setCount] = useState(0);
+  const obj = useMemo(() => ({ currenCount: 0 }), []);
   const isInit = useRef(true);
   const intervalId = useRef(0);
 
+  obj.currenCount = count;
   if (isInit.current) {
     intervalId.current = setInterval(() => {
-      setCount(count + 1);
+      setCount(obj.currenCount + 1);
     }, 1000);
     isInit.current = false;
   }
 
   useEffect(() => {
-    return clearInterval(intervalId.current);
+    return () => clearInterval(intervalId.current);
   }, []);
 
   return count;
