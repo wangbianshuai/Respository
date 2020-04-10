e23b48a33f722f9342ca6bb1c660ebb0f1414f13
wangbianzhai <277822695@qq.com>
2020-04-10 21:39:06 +0800

    checkin

--- WebHook/src/tests/useAddComponent.js
+++ WebHook/src/tests/useAddComponent.js
@@ -1,18 +1,13 @@
-import { useState, useMemo, useCallback } from "react";
+import { useState, useMemo } from "react";
 
 export default props => {
   const [count, setCount] = useState(0);
   const obj = useMemo(() => ({ isFirst: true }), [])
 
-  const addComponent = useCallback(() => {
-    setTimeout(() => {
-      setCount(count + 1);
-      addComponent();
-    }, 1000);
-  }, [count]);
-
   if (obj.isFirst) {
-    addComponent();
+    setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
     obj.isFirst = false;
   }
 
