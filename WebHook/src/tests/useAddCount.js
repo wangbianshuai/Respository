3c2377a56c6183749ea0c8f792c2ec5fc17c4dee
wangbianzhai <277822695@qq.com>
2020-04-10 21:48:41 +0800

    checkin

--- WebHook/src/tests/useAddCount.js
+++ WebHook/src/tests/useAddCount.js
@@ -1,15 +1,20 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default props => {
   const [count, setCount] = useState(0);
-  const isInit = useRef(true)
+  const isInit = useRef(true);
+  const intervalId = useRef(0);
 
   if (isInit.current) {
-    setInterval(() => {
+    intervalId.current = setInterval(() => {
       setCount(count + 1);
     }, 1000);
     isInit.current = false;
   }
+
+  useEffect(() => {
+    return clearInterval(intervalId.current);
+  }, []);
 
   return count;
 }
