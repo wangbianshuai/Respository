854dc1140aa5de2f090e7a62a0262784fc1e5305
wangbianzhai <277822695@qq.com>
2020-06-19 22:10:46 +0800

    checkin

--- reacthooks618/src/actions/actionTypes/components/dataGridView.js
+++
@@ -1,7 +0,0 @@
-//dataGridView 300-399
-export default {
-  //搜索查询
-  searchQuery: 300,
-  //Excel导出
-  excelExport: 301
-}
--- reacthooks618/src/actions/actionTypes/login.js
+++
@@ -1,5 +0,0 @@
-//login 4000-4099
-export default {
-  //login
-  login: 4000
-}
